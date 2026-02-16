60fb3fd9e7c3e85924875a382e82003b31e8d550
mrbtools <124554185+mrbtool@users.noreply.github.com>
2026-02-16 18:42:10 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'rickshaw-go-v1';
+const urlsToCache = [
+  './index.html',
+  './garo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
